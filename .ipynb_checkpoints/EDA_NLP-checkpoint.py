076ed9aae9d7cc19185dc9fab5cdc5f0d6bf70c5
Kyle Pridgen <wkylepridgen@gmail.com>
2024-05-17 16:52:28 -0400

    NDA NLP
    Tried to upload scatter text. Do not understand code for errors.

--- .ipynb_checkpoints/EDA_NLP-checkpoint.py
+++ .ipynb_checkpoints/EDA_NLP-checkpoint.py
@@ -1,7 +1,42 @@
 import streamlit as st
+import streamlit.components.v1 as components
 import pandas as pd
 import numpy as np
+import os
+import joblib
+from sklearn.metrics import classification_report, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
-import seaborn as sns
-from io import StringIO
-st.title('Word Clouds')
+import numpy as np
+import json
+from wordcloud import WordCloud
+from nltk import casual_tokenize
+# Changing the Layout
+st.set_page_config( layout="wide")
+
+
+with open('config/filepaths.json') as f:
+    FPATHS = json.load(f)
+
+st.title("Exploratory Data Analysis of Movie Reviews")
+
+st.divider()
+st.subheader("ScatterText:")
+@st.cache_data
+def load_scattertext(fpath):
+    with open(fpath) as f:
+        explorer = f.read()
+        return explorer
+
+
+# Load scattertext from filepaths
+html_to_show = load_scattertext(FPATHS['eda']['scattertext'])
+
+# Checkbox to trigger display of scattertext
+checkbox_scatter = st.checkbox("Show Scattertext Explorer",value=False)
+
+
+if checkbox_scatter:     
+    components.html(html_to_show, width=1200, height=800, scrolling=True)
+else:
+    st.empty()
+
